cf5a8ba76ab1fb92f5637ec159919a3fee967194
guillermoverdu <ext-guillermo.verdu@elisa.fi>
2019-09-20 13:37:00 +0300

    First response

--- src/main/java/elhombreseta/bot/botcierto/controller/Controller.java
+++ src/main/java/elhombreseta/bot/botcierto/controller/Controller.java
@@ -1,5 +1,8 @@
 package elhombreseta.bot.botcierto.controller;
 
+import elhombreseta.bot.botcierto.model.Update;
+import elhombreseta.bot.botcierto.rest.Post;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/bot")
 public class Controller {
 
+    private Post post;
+
+    public Controller(Post post) {
+        this.post = post;
+    }
+
     @RequestMapping(value = "/botcierto", method = RequestMethod.GET)
     public String getBotciertos() {
+        return "{botcierto}";
+    }
+
+    @RequestMapping(value = "/botcierto", method = RequestMethod.POST)
+    public String postBotciertos(@RequestBody Update update) {
+        post.post(update.getMessage().getText(), update.getMessage().getChat().getId());
         return "{botcierto}";
     }
 }
---
+++ src/main/java/elhombreseta/bot/botcierto/model/Response.java
@@ -0,0 +1,19 @@
+package elhombreseta.bot.botcierto.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.io.Serializable;
+
+public class Response implements Serializable {
+
+    @JsonProperty("text")
+    private final String text;
+
+    @JsonProperty("chat_id")
+    private final Integer chat_id;
+
+    public Response(String text, Integer chat_id) {
+        this.text = text;
+        this.chat_id = chat_id;
+    }
+}
